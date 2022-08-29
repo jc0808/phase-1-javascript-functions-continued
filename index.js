f7ad7df889c38bb3c4069f161eefbf49884afba0
Joshua Campos <jdeveloper1203@gmail.com>
2022-08-29 12:12:07 -0400

    commit tree b4ed42b74f3b2e170d26bb271b555389c6874d77

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(value = "roller-skate"){
+    return `This Saturday, I want to ${value}!`;
+};
+
+function mondayWork(value = "go to the office"){
+    return `This Monday, I will ${value}.`;
+};
+
+function wrapAdjective(string = "*"){
+    const innerFuction = (value = "special") => {
+        return `You are ${string}${value}${string}!`;
+    };
+
+    return innerFuction;
+};
